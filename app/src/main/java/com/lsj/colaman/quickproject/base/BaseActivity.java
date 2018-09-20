60ebc91298e4daf16cb11fb17b44ab075959482d
kyle <kyle@ganguo.hk>
2018-09-20 18:39:52 +0800

    feat:增加屏幕适配方案

---
+++ app/src/main/java/com/lsj/colaman/quickproject/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.lsj.colaman.quickproject.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Create by kyle on 2018/9/19
+ * Function : baseActivity
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
